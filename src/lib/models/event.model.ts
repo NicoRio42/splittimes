4e5e3579f65294c1fc2c2fb1f9afc0ec026f299b
Nicolas Rio <nicolas.rio42@gmail.com>
2023-04-13 23:06:03 +0200

    Events table

--- src/lib/models/event.model.ts
+++ src/lib/models/event.model.ts
@@ -1,4 +1,7 @@
 export interface Event {
 	id: string;
 	name: string;
+	type: string;
+	organiser: string;
+	country: string;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,28 +5,64 @@
 	export let data;
 
 	let events: Event[] = [];
+	let filteredEvents: Event[] = [];
+	let filter = '';
 
-	$: if (browser) {
+	$: if (browser) initEvents(data.events);
+
+	function initEvents(eventsString: string) {
 		const parser = new DOMParser();
-		const xmlDoc = parser.parseFromString(data.events, 'text/xml');
+		const xmlDoc = parser.parseFromString(eventsString, 'text/xml');
 		const eventTags = xmlDoc.querySelectorAll('Event');
 
 		events = Array.from(eventTags).map((eventTag) => {
 			const idTag = eventTag.querySelector('Id');
 			const nameTag = eventTag.querySelector('Name');
+			const typeTag = eventTag.querySelector('Classification');
+			const organiserTag = eventTag.querySelector('Organiser Name');
+			const countryTag = eventTag.querySelector('Organiser Country');
 
-			if (idTag === null || nameTag === null) throw new Error('Problem with file format');
+			if (
+				idTag === null ||
+				nameTag === null ||
+				typeTag === null ||
+				organiserTag === null ||
+				countryTag === null
+			)
+				throw new Error('Problem with file format');
 
 			const id = idTag.textContent;
 			const name = nameTag.textContent;
+			const type = typeTag.textContent;
+			const organiser = organiserTag.textContent;
+			const country = countryTag.getAttribute('code');
 
-			if (id === null || name === null) throw new Error('Problem with file format');
+			if (id === null || name === null || type === null || organiser === null || country === null)
+				throw new Error('Problem with file format');
 
 			return {
 				id,
-				name
+				name,
+				type,
+				organiser,
+				country
 			};
 		});
+
+		filteredEvents = [...events];
+		filter = '';
+	}
+
+	function handleFilter() {
+		const lowercaseFilter = filter.toLowerCase();
+
+		filteredEvents = events.filter(
+			(event) =>
+				event.country.toLowerCase().includes(lowercaseFilter) ||
+				event.name.toLowerCase().includes(lowercaseFilter) ||
+				event.type.toLowerCase().includes(lowercaseFilter) ||
+				event.organiser.toLowerCase().includes(lowercaseFilter)
+		);
 	}
 </script>
 
@@ -35,21 +71,51 @@
 		<label>
 			Date
 
-			<input type="date" name="date" value={data.date} onchange="this.form.submit()" />
+			<input class="date" type="date" name="date" value={data.date} onchange="this.form.submit()" />
 		</label>
 	</form>
 
-	<ul>
-		{#each events as event (event.id)}
-			<li><a href={`/events/${event.id}`}> {event.name} </a></li>
-		{/each}
-	</ul>
+	<label class="search">
+		Search
+
+		<input name="search" bind:value={filter} on:input={handleFilter} />
+	</label>
+
+	<figure>
+		<table>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Type</th>
+					<th>Organiser</th>
+					<th>Country</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				{#each filteredEvents as event (event.id)}
+					<tr>
+						<td><a href={`/events/${event.id}`}> {event.name} </a></td>
+						<td>{event.type}</td>
+						<td>{event.organiser}</td>
+						<td>{event.country}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</figure>
 </main>
 
 <style>
-	.form {
+	.form,
+	.search {
 		max-width: 20rem;
 		margin-left: auto;
 		margin-right: auto;
+	}
+
+	.form,
+	.date {
+		margin-bottom: 0;
 	}
 </style>
