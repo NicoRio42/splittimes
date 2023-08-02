c7a825bcd5750102920bccc07bcb00bcfe18631c
Nicolas Rio <nicolas.rio@cgi.com>
2023-08-02 18:26:16 +0200

    Revert "Event backlink"
    This reverts commit ada5acffdf18ae887431b83431e734d7e75da39a.

--- src/routes/[provider]/[eventId]/classes/[classId]/+layout.svelte
+++ src/routes/[provider]/[eventId]/classes/[classId]/+layout.svelte
@@ -1,22 +1,14 @@
 <script lang="ts">
 	import { afterNavigate } from '$app/navigation';
 	import { page } from '$app/stores';
-	export let data;
 
 	const baseUrl = `/${$page.params.provider}/${$page.params.eventId}/classes/${$page.params.classId}`;
 	let detailsElement: HTMLDetailsElement;
-
-	$: event = data.events.find((e) => e.id === $page.params.eventId);
 
 	afterNavigate(() => detailsElement?.removeAttribute('open'));
 </script>
 
 <div>
-	{#if event !== undefined}
-		<p class="event-link">
-			Event: <a href="/{$page.params.provider}/{event.id}">{event.name}</a>
-		</p>
-	{/if}
 	<details role="list" bind:this={detailsElement}>
 		<summary aria-haspopup="listbox">
 			{#if $page.url.pathname.includes('superman-graph')}
@@ -43,11 +35,6 @@
 <slot />
 
 <style>
-	.event-link {
-		text-align: center;
-		margin: 0;
-	}
-
 	details {
 		margin: 0.25rem;
 	}
